380a5db54e7ffde573b0ffbc3cebffa5676745a9
Pablo Cases <cases.pablo@gmail.com>
2022-12-01 21:20:20 +0100

    ultraheat.h: Robust reading of wakeup message on dedicated UART

--- ultraheat.h
+++ ultraheat.h
@@ -66,7 +66,7 @@ class UltraheatMessage {
 
 class UltraheatReader : public PollingComponent, public UARTDevice {
     //const char* DELIMITERS = "()*:";
-    char buffer[BUF_SIZE];
+    uint8_t buffer[BUF_SIZE];
 
     public:
         //Sensor *model= new Sensor();
@@ -104,7 +104,7 @@ class UltraheatReader : public PollingComponent, public UARTDevice {
         Sensor *ownership_number = new Sensor();
         Sensor *settings_and_firmware = new Sensor();
 
-        UltraheatReader(UARTComponent *parent) : PollingComponent(5000), UARTDevice(parent)  {}
+        UltraheatReader(UARTComponent *parent) : PollingComponent(20000), UARTDevice(parent)  {}
 
         float get_setup_priority() const override {
             return esphome::setup_priority::LATE;
@@ -248,83 +248,186 @@ class UltraheatReader : public PollingComponent, public UARTDevice {
         void readUltraheatMessage() {
 
             // wakeup at 300bps
-            ESP_LOGD("wakeup", "send");
+            ESP_LOGD("wakeup", "start");
+            ESP8266UartComponent *_parent = (ESP8266UartComponent *)parent_;
+            //_parent->dump_config();
+            //TODO why are we booting at 2400 on debug uart?, yaml and previously we booted at 300...
 
+            // clear RX buffer, may contain a mix of 300 and 2400 bps data, just ignore
+            int len;
+            do {
+                len = available();
+                // avoid buffer overrun
+                if (len >= BUF_SIZE) {
+                    len = BUF_SIZE;
+                }
+                ESP_LOGD("wakeup", "trash: %d", len);
+                read_array(buffer, len);
+            } while (len);
+
+            ESP_LOGD("wakeup", "change baud rate -> 300");
             // https://esphome.io/api/classesphome_1_1uart_1_1_u_a_r_t_component.html
             //set_baud_rate(300); // compile error
             //UARTComponent::set_baud_rate(300); //cannot call member function without object
             //parent_->set_baud_rate(300);
 
+            _parent->set_baud_rate(300);
+            _parent->setup();
+            //_parent->dump_config();
+
             //home/backloop/.platformio/packages/framework-arduinoespressif8266/cores/esp8266/uart.h:125:6: note: declared here
             // void uart_set_baudrate(uart_t* uart, int baud_rate);
             //uart_set_baudrate(uart, 300);
+
+            ESP_LOGD("wakeup", "send wakeup");
+            // "<bunch of zeroes>/?!\r\n"
             const uint8_t wakeup[] ="\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2F\x3F\x21\x0D\x0A";
             write_array(&wakeup[0], sizeof(wakeup));
 
-            // readback device name  or just idle...
-            delay(500); //checked by logic analyzer
-            if (available()) {
-                int len = Serial.readBytesUntil('\n', buffer, BUF_SIZE);
+            // readback device name
+            const uint8_t prod_str[] = "/LUGCUH50\r\n";
+
+            // initialize buffer
+            memset (buffer, 0, BUF_SIZE);
+
+            uint8_t iterations = 0;
+            int old_cnt = 0;
+            uint8_t *write = buffer;
+            while (strcmp((char *)buffer, (char *)prod_str) != 0) {
+
+                // wait for slow 300bps chars to arrive
+                delay(100);
+
+                uint8_t len = available();
 
                 if (len > 0) {
-                    ESP_LOGD("wakeup", "response: %s", buffer);
+                    //ESP_LOGD("wakeup", "buffer: %p, write: %p", buffer, write);
+                    //ESP_LOGD("wakeup", "available: %d", len);
+                    bool success = read_array(write, len);
+                    // TOD: check success == false for timeout...
+                    // TODO: buffer overrun, check len...
+
+                    // terminate string
+                    *(write + len) = 0;
+
+                    //ESP_LOGD("wakeup", "read len: %d", len);
+                    //ESP_LOGD("wakeup", "new: <%s>", buffer);
+
+                    // find last ocurrence of start character
+                    char *start = strrchr((char *)buffer, prod_str[0]);
+
+                    // find last ocurrence of end character
+                    char *end = strrchr((char *)buffer, prod_str[sizeof(prod_str)-1]);
+
+                    //ESP_LOGD("wakeup", "buffer: %p, write: %p, start: %p, end: %p", buffer, write, start, end);
+                    //ESP_LOGD("wakeup", "buffer: %p, write: %p -> len: %lu", buffer, write, write-buffer);
+                    //for (uint8_t *c = buffer; c < (write+len); c++) {
+                    //    ESP_LOGD("wakep", "0x%x", *c);
+                    //}
+
+                    if (start == NULL) {
+                        start = (char *)buffer;
+                    }
+
+                    if (end == NULL) {
+                        end = (char *)(write + len);
+                    }
+                    //ESP_LOGD("wakeup", "buffer: %p, write: %p, start: %p, end: %p, len: %lu", buffer, write, start, end, end - start);
+
+                    // cut away surrounding noise
+                    memmove(buffer, start, end - start);
+
+                    // set new write pointer
+                    write = (uint8_t *)(buffer + (size_t)(end - start));
+                    //write += len;
+
+                    // terminate string
+                    *write = 0;
+                    ESP_LOGD("wakeup", "stripped: <%s>", buffer);
+                }
+
+                int cnt = 0;
+                for (cnt = 0; cnt < sizeof(prod_str); cnt++) {
+                    if (buffer[cnt] != prod_str[cnt]) break;
+                }
+                if (cnt == old_cnt) {
+                    iterations++;
+                    if (iterations >= 10) {
+                        ESP_LOGD("wakeup", "timeout...");
+                        return;
+                    }
+                } else {
+                    // assume matching chars don't decrease over time
+                    old_cnt = cnt;
+                    iterations = 0;
                 }
             }
 
-            ESP_LOGD("data", "change baud rate");
-            // read message at 2400bps
-            parent_->set_baud_rate(2400);
-            //Serial.set_baud_rate(2400);
+            // trim trailing characters
+            // use strtok() side effect that it replaces first occurrence of delimiter with '\0'
+            strtok((char *)buffer, "\r\n");
 
-            if (available()) {
+            ESP_LOGD("wakeup", "product: <%s>", buffer);
+            return;
 
-                UltraheatMessage message = UltraheatMessage();
-                unsigned int wait_time = 0;
-                bool complete = false;
-
-                while (wait_time < 1000) {
-
-                    int len = Serial.readBytesUntil('\n', buffer, BUF_SIZE);
-
-                    if (len > 0) {
-                        ESP_LOGD("data", "%s", buffer);
-
-                        if (buffer[0] == '!') {
-                            complete = true;
-                            break;
-                        }
-
-                        // row might contain multiple data items with dirrerent format
-                        // common parts are <obisCode>(<value>)
-                        // <value>, may contain multiple data points separated by '&'
-                        // if multiple data points exist, then each might contain a
-                        // corresponding <unit>
-                        if (strchr(buffer, '(') != NULL) {
-                            char *saveptr;
-                            char* obisCode = strtok_r(buffer, "(", &saveptr);
-                            char* value = strtok_r(NULL, ")", &saveptr);
-                            ESP_LOGD("data", "%s, %s", obisCode, value);
-                            //do {
-                            //    parseReponseBlock(&message, obisCode, value);
-                            //    obisCode = strtok_r(NULL, "(", &saveptr);
-                            //    value = strtok_r(NULL, ")", &saveptr);
-                            //} while (obisCode != NULL);
-                        }
-                    }
-                    // clean buffer
-                    memset(buffer, 0, BUF_SIZE - 1);
-
-                    if (!available()) {
-                        // wait for more data
-                        int time = 40;
-                        delay(40);
-                        wait_time += time;
-                    }
-                }
-
-                if (complete) {
-                    publishSensors(&message);
-                }
-            }
+//            ESP_LOGD("data", "change baud rate -> 2400");
+//            // read message at 2400bps
+//            //Serial.set_baud_rate(2400);
+//            //parent_->set_baud_rate(2400); //compiles, but with no effect as UartComponent->set_baud_rate() only stores this data in RAM..
+//            _parent->set_baud_rate(2400);
+//            _parent->setup();
+//            //_parent->dump_config();
+//
+//            //delay(5000);
+//            if (0) { //available()) {
+//
+//                UltraheatMessage message = UltraheatMessage();
+//                unsigned int wait_time = 0;
+//                bool complete = false;
+//
+//                while (wait_time < 1000) {
+//
+//                    int len = Serial.readBytesUntil('\n', buffer, BUF_SIZE);
+//
+//                    if (len > 0) {
+//                        ESP_LOGD("data", "%s", buffer);
+//
+//                        if (buffer[0] == '!') {
+//                            complete = true;
+//                            break;
+//                        }
+//
+//                        // row might contain multiple data items with dirrerent format
+//                        // common parts are <obisCode>(<value>)
+//                        // <value>, may contain multiple data points separated by '&'
+//                        // if multiple data points exist, then each might contain a
+//                        // corresponding <unit>
+//                        if (strchr(buffer, '(') != NULL) {
+//                            char *saveptr;
+//                            char* obisCode = strtok_r(buffer, "(", &saveptr);
+//                            char* value = strtok_r(NULL, ")", &saveptr);
+//                            ESP_LOGD("data", "%s, %s", obisCode, value);
+//                            //do {
+//                            //    parseReponseBlock(&message, obisCode, value);
+//                            //    obisCode = strtok_r(NULL, "(", &saveptr);
+//                            //    value = strtok_r(NULL, ")", &saveptr);
+//                            //} while (obisCode != NULL);
+//                        }
+//                    }
+//                    // clean buffer
+//                    memset(buffer, 0, BUF_SIZE - 1);
+//
+//                    if (!available()) {
+//                        // wait for more data
+//                        int time = 40;
+//                        delay(40);
+//                        wait_time += time;
+//                    }
+//                }
+//
+//                if (complete) {
+//                    publishSensors(&message);
+//                }
+//            }
         }
 };
