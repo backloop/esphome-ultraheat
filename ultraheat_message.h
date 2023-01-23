f837333b104a648e889d46a4ad0844a41af79365
Pablo Cases <cases.pablo@gmail.com>
2023-01-23 21:50:57 +0100

    ultraheat_message: Adding Ultraheat namespace

--- ultraheat_message.h
+++ ultraheat_message.h
@@ -8,77 +8,80 @@
 #define UM_PRINTF(...)
 #endif
 
-class UltraheatMessage {
+namespace Ultraheat {
 
-    public:
-        //double model;
-        double power_max_kw;
-        double power_max_previous_year_kw;
+    class UltraheatMessage {
 
-        double heat_usage_mwh;
-        double heat_previous_year_mwh;
+        public:
+            //double model;
+            double power_max_kw;
+            double power_max_previous_year_kw;
 
-        double volume_usage_m3;
-        double volume_previous_year_m3;
+            double heat_usage_mwh;
+            double heat_previous_year_mwh;
 
-        double error_number;
-        double fault_hours;
-        double fault_hours_previous_year;
+            double volume_usage_m3;
+            double volume_previous_year_m3;
 
-        double flowrate_max_m3ph;
-        double flowrate_max_previous_year_m3ph;
-        double measuring_range_m3ph;
+            double error_number;
+            double fault_hours;
+            double fault_hours_previous_year;
 
-        std::string meter_date_time;
-        double measurement_period_minutes;
-        std::string yearly_set_day;
-        std::string monthly_set_day;
-        double operating_hours;
-        double flow_hours;
+            double flowrate_max_m3ph;
+            double flowrate_max_previous_year_m3ph;
+            double measuring_range_m3ph;
 
-        double flow_temperature_max_c;
-        double return_temperature_max_c;
-        double flow_temperature_max_previous_year_c;
-        double return_temperature_max_previous_year_c;
+            std::string meter_date_time;
+            double measurement_period_minutes;
+            std::string yearly_set_day;
+            std::string monthly_set_day;
+            double operating_hours;
+            double flow_hours;
 
-        std::string device_number;
-        std::string ownership_number;
-        std::string settings_and_firmware;
+            double flow_temperature_max_c;
+            double return_temperature_max_c;
+            double flow_temperature_max_previous_year_c;
+            double return_temperature_max_previous_year_c;
 
-        void print() {
-            UM_PRINTF("power_max_kw                            %f", power_max_kw);
-            UM_PRINTF("power_max_previous_year_kw              %f", power_max_previous_year_kw);
+            std::string device_number;
+            std::string ownership_number;
+            std::string settings_and_firmware;
 
-            UM_PRINTF("heat_usage_mwh                          %f", heat_usage_mwh);
-            UM_PRINTF("heat_previous_year_mwh                  %f", heat_previous_year_mwh);
+            void print() {
+                UM_PRINTF("power_max_kw                            %f", this->power_max_kw);
+                UM_PRINTF("power_max_previous_year_kw              %f", this->power_max_previous_year_kw);
 
-            UM_PRINTF("volume_usage_m3                         %f", volume_usage_m3);
-            UM_PRINTF("volume_previous_year_m3                 %f", volume_previous_year_m3);
+                UM_PRINTF("heat_usage_mwh                          %f", this->heat_usage_mwh);
+                UM_PRINTF("heat_previous_year_mwh                  %f", this->heat_previous_year_mwh);
 
-            UM_PRINTF("error_number                            %f", error_number);
-            UM_PRINTF("fault_hours                             %f", fault_hours);
-            UM_PRINTF("fault_hours_previous_year               %f", fault_hours_previous_year);
+                UM_PRINTF("volume_usage_m3                         %f", this->volume_usage_m3);
+                UM_PRINTF("volume_previous_year_m3                 %f", this->volume_previous_year_m3);
 
-            UM_PRINTF("flowrate_max_m3ph                       %f", flowrate_max_m3ph);
-            UM_PRINTF("flowrate_max_previous_year_m3ph         %f", flowrate_max_previous_year_m3ph);
-            UM_PRINTF("measuring_range_m3ph                    %f", measuring_range_m3ph);
+                UM_PRINTF("error_number                            %f", this->error_number);
+                UM_PRINTF("fault_hours                             %f", this->fault_hours);
+                UM_PRINTF("fault_hours_previous_year               %f", this->fault_hours_previous_year);
 
-            UM_PRINTF("meter_date_time                         %s", meter_date_time.c_str());
-            UM_PRINTF("measurement_period_minutes              %f", measurement_period_minutes);
-            UM_PRINTF("yearly_set_day                          %s", yearly_set_day.c_str());
-            UM_PRINTF("monthly_set_day                         %s", monthly_set_day.c_str());
-            UM_PRINTF("operating_hours                         %f", operating_hours);
-            UM_PRINTF("flow_hours                              %f", flow_hours);
+                UM_PRINTF("flowrate_max_m3ph                       %f", this->flowrate_max_m3ph);
+                UM_PRINTF("flowrate_max_previous_year_m3ph         %f", this->flowrate_max_previous_year_m3ph);
+                UM_PRINTF("measuring_range_m3ph                    %f", this->measuring_range_m3ph);
 
-            UM_PRINTF("flow_temperature_max_c                  %f", flow_temperature_max_c);
-            UM_PRINTF("return_temperature_max_c                %f", return_temperature_max_c);
-            UM_PRINTF("flow_temperature_max_previous_year_c    %f", flow_temperature_max_previous_year_c);
-            UM_PRINTF("return_temperature_max_previous_year_c  %f", return_temperature_max_previous_year_c);
+                UM_PRINTF("meter_date_time                         %s", this->meter_date_time.c_str());
+                UM_PRINTF("measurement_period_minutes              %f", this->measurement_period_minutes);
+                UM_PRINTF("yearly_set_day                          %s", this->yearly_set_day.c_str());
+                UM_PRINTF("monthly_set_day                         %s", this->monthly_set_day.c_str());
+                UM_PRINTF("operating_hours                         %f", this->operating_hours);
+                UM_PRINTF("flow_hours                              %f", this->flow_hours);
 
-            UM_PRINTF("device_number                           %s", device_number.c_str());
-            UM_PRINTF("ownership_number                        %s", ownership_number.c_str());
-            UM_PRINTF("settings_and_firmware                   %s", settings_and_firmware.c_str());
-        }
+                UM_PRINTF("flow_temperature_max_c                  %f", this->flow_temperature_max_c);
+                UM_PRINTF("return_temperature_max_c                %f", this->return_temperature_max_c);
+                UM_PRINTF("flow_temperature_max_previous_year_c    %f", this->flow_temperature_max_previous_year_c);
+                UM_PRINTF("return_temperature_max_previous_year_c  %f", this->return_temperature_max_previous_year_c);
+
+                UM_PRINTF("device_number                           %s", this->device_number.c_str());
+                UM_PRINTF("ownership_number                        %s", this->ownership_number.c_str());
+                UM_PRINTF("settings_and_firmware                   %s", this->settings_and_firmware.c_str());
+            }
+    };
 };
 
 #endif // ULTRAHEAT_MESSAGE
