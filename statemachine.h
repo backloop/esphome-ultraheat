a831ec72744c956b66ac5bba92028a16d78cdd78
Pablo Cases <cases.pablo@gmail.com>
2023-04-02 08:27:47 +0200

    statemachine: Add descriptions of State actions

--- statemachine.h
+++ statemachine.h
@@ -19,8 +19,15 @@ namespace StateMachine {
                 : next(nullptr)
             {}
 
+            // executed when leaving a state
             virtual void enter() { SM_PRINTF("State::enter()"); };
+
+            // periodically called
+            // return: false - while exit condition is not met
+            //         true - when exit condition met
             virtual bool tick() = 0;
+
+            // executed when entering a state
             virtual void exit() { SM_PRINTF("State::exit()"); };
 
             void set_next(State* state) {
